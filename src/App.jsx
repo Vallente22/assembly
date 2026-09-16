4607e6035b6568ddfff188463d54ed4785cbe66d
val <vallentenico21@gmail.com>
2026-09-16 14:56:28 +0800

    project planning answer

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,19 @@ import './App.css'
  * 
  * - What are the main containers of elements I need
  *   in this app?
- * 
+ * - container for the life left, alphabet letters to know if the 
+ *   letter is right or wrong and the input text so we can see the word we typed 
  * 
  * - What values will need to be saved in state vs.
  *   what values can be derived from the state?
+ * - state we will be needing are the input one, and for the right and wrong 
+ *   letters as well maybe it can be derived from the input state
  * 
  * 
  * - How will the user interact with the app? What
  *   events do I need to handle?
+ * - events i need to handle are obviously one for new game button and on change
+ *   for the input
  * 
  * 
  */
